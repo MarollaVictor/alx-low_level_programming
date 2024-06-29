65eb80273b26d03a63fd505353491e75af8c8be4
MarollaVictor <marollavictorsedky@gmail.com>
2024-06-29 13:38:02 -0700

    Time table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,12 +14,8 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-<<<<<<< HEAD
-=======
 void print_to_98(int n);
-
->>>>>>> 156308e427ddd22b65e4da7089caf9ddc88c5ab1
-
+void print_times_table(int n);
 
 
 #endif
