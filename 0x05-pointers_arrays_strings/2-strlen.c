f99949a63838a5f5d1ee9e20102dc7a76ed9f642
MarollaVictor <marollavictorsedky@gmail.com>
2024-07-01 11:52:18 -0700

    Returnning length of string through poointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of an string
+ *
+ * @s: the given string
+ *
+ * Return: length of the string
+*/
+
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (counter != "\0")
+		++counter;
+
+	return (counter);
+}
