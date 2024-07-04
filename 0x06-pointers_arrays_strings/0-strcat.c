3e3fc884131a28fae4d055bfa4d2fcd8998f5113
MarollaVictor <marollavictorsedky@gmail.com>
2024-07-04 16:15:35 -0700

    first project

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,20 +5,27 @@
  * @dest: pointer to destination input
  * @src: pointer to source input
  *
- * Return: pointer to returnning @dest
+ * Return: void
 */
 
 char *_strcat(char *dest, char *src)
 {
-	int c, c1;
+	int i, j;
 
-	c = 0;
+	i = 0;
 	/*find the size of the dest array*/
-	while (dest[c])
-		c++;
+	while (dest[i] != '\0')
+		i++;
 
 	/*iterate through each src array value without the null byte*/
-	for (c2 = 0; src[c2]; c2++)
-		dest[c++] = src[c2];
-	return (dest):
+	j = 0;
+	while(src[j] != '\0')
+	{
+		dest[i] = src [j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
