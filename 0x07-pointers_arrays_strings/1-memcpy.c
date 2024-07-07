461af6f368f56ecdca23a6151a2b5c30c7568e5d
MarollaVictor <marollavictorsedky@gmail.com>
2024-07-07 06:49:04 -0700

    memory address copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,9 +13,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int itr;
 
-	for (itr = 0; itr < n; itr++, n--)
+	for (itr = 0; itr < n; itr++)
 	{
-		dest[itr] += src[itr];
+		dest[itr] = src[itr];
 	}
+	_putchar('\n');
+
 	return (dest);
 }
