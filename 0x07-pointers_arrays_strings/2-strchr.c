dd945de4a16a9336d83104d9de2a641acb55fcb2
MarollaVictor <marollavictorsedky@gmail.com>
2024-07-08 03:57:34 -0700

    sc

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_strchr - fills memory with a constant byte
+ * *_strchr - fills memory with a constan:t byte
  * @s: pointer to put the constant
  * @c: constant
  * Return: pointer to a
@@ -11,12 +11,10 @@ char *_strchr(char *s, char c)
 {
 	int itr;
 
-	for (itr = 0; s[itr] != '\0'; itr++)
+	for (itr = 0; s[itr] >= '\0'; itr++)
 	{
-		if (s[itr] == 'c')
-		{
+		if (s[itr] == c)
 			return (s + itr);
-		}
 	}
 
 	return ('\0');
