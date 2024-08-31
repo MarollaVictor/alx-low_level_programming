4c630a597df7c201668a7edd2e493eee298071e3
MarollaVictor <marollavictorsedky@gmail.com>
2024-08-31 21:24:20 +0300

    binary to string

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/*
+ *  _putchar writes the char c to the standard output
+ *  c: The character to be printed
+ *  Return : on success 1
+ *  on error -1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
